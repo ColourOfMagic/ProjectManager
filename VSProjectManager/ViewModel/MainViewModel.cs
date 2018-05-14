e8ab4f20c4c3ac0469ea09b7b3c57445af2b8d33
dalv rinsvind <v.dalv@outlook.com>
2018-05-14 23:10:44 +0300

    Start

---
+++ VSProjectManager/ViewModel/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using VSProjectManager.Model;
+
+namespace VSProjectManager.ViewModel
+{
+    class MainViewModel : ViewModelBase
+    {
+        ObservableCollection<Project> projects;
+        public ObservableCollection<Project> Projects
+        {
+            get
+            {
+                return projects;
+            }
+            set
+            {
+                // OnPropertyChanged("Projects");  //заменить потом аналогом через MVVM light
+                Set(ref projects, value);
+            }
+
+        }
+
+        public MainViewModel()
+        {
+            Projects = new ObservableCollection<Project>
+            {
+                new Project("Dir1","dd//rr.tct",DateTime.Now,true),
+                new Project("Dir2","ht//rk.tat",DateTime.Now,true)
+            };
+        }
+
+    }
+}
